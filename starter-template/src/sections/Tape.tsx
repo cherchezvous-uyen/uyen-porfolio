86e1800a33ec58a07843d57b5fa245cb241a76ec
uyenmonica <nguyenvotuuyen.work@gmail.com>
2025-08-16 00:03:26 +0700

    done tape

--- starter-template/src/sections/Tape.tsx
+++ starter-template/src/sections/Tape.tsx
@@ -1,3 +1,19 @@
+import StarIcon from '@/assets/icons/star.svg';
+const words= ["Performance", "Scalability", "Reliability", "Innovation", "Efficiency", "Sustainability", "Collaboration", "Agility", "Security", "Flexibility"];
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+  <div className='py-16 lg:py-24'>
+    <div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1'>
+      <div className='flex [mask-image:linear-gradient(to right, transparent, black_10%, black_90%, transparent)]'>
+        <div className='flex flex-none gap-4 py-3'>{words.map(word=> (
+          <div key={word} className='inline-flex gap-4 items-center' >
+            <span className='text-gray-900 uppercase font-extrabold text-sm'>{word}</span>
+            <StarIcon className="size-6 text-gray-900"/>
+          </div>
+    ))}
+        </div>
+      </div>
+    </div>
+  </div>
+  );
 };
