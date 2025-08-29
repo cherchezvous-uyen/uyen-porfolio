87c258c13f2c65c77e4b85e651bf693e7f006b75
uyenmonica <nguyenvotuuyen.work@gmail.com>
2025-08-29 23:49:47 +0700

    done tape

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -1,4 +1,10 @@
 import {SectionHeader} from "@/components/SectionHeader";
+import {Card} from "@/componets/Card"
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return( 
+  <div className="pb-96">
+    <SectionHeader eyebrow="About Me" title="A Glimpse Into My World" description="Learn more about who I am, What I do"/>
+    div
+  </div>
+  );
 };
